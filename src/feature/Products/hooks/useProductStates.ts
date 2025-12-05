1adad003cf44258c3d1cc49411161951ff7c2546
androy <androyt1@gmail.com>
2025-12-05 15:58:05 +0000

    added the sort state

--- src/feature/Products/hooks/useProductStates.ts
+++ src/feature/Products/hooks/useProductStates.ts
@@ -1,9 +1,11 @@
 import { useState, type ChangeEvent } from "react";
+import { type sortOption } from "../components/types";
 
 export const useProductStates = () => {
   const [search, setSearch] = useState<string>("");
   const [category, setCategory] = useState<string>("all");
   const [priceRange, setPriceRange] = useState<string>("All items");
+  const [sort, setSort] = useState<sortOption>("lowest to highest");
 
   const handleSelectCategory = (e: ChangeEvent<HTMLSelectElement>) => {
     setCategory(e.target.value);
@@ -16,12 +18,19 @@ export const useProductStates = () => {
   const handleSelectPriceRange = (e: ChangeEvent<HTMLSelectElement>) => {
     setPriceRange(e.target.value);
   };
+
+  const handleSortOption = (e: ChangeEvent<HTMLSelectElement>) => {
+    setSort(e.target.value as sortOption);
+  };
+
   return {
     search,
     category,
     priceRange,
+    sort,
     handleSelectCategory,
     handleSearch,
     handleSelectPriceRange,
+    handleSortOption,
   };
 };
