af8213dc29ea1cb254fcf14e15328244c77ea3db
androy <androyt1@gmail.com>
2025-12-05 15:57:04 +0000

    added the sort filter component

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,10 @@ import {
   PriceFilter,
   InputFilter,
   useProductStates,
+  SortFilter,
 } from "./feature/Products";
 import Layout from "./components/Layout";
+import { sortByPrice } from "./feature/Products/utilities";
 
 import {
   managePriceFilter,
@@ -21,22 +23,26 @@ const App = () => {
     search,
     category,
     priceRange,
+    sort,
     handleSelectCategory,
     handleSearch,
     handleSelectPriceRange,
+    handleSortOption,
   } = useProductStates();
 
   const categoryFilterProps = { category, handleSelectCategory };
   const priceFilterProps = { priceRange, handleSelectPriceRange };
   const inputFilterProps = { search, handleSearch };
+  const SortFilterProps = { sort, handleSortOption };
 
   const filteredProducts = useMemo(() => {
     let items = products;
     items = filterByCategory(items, category);
     items = handleUserSearch(items, search);
     items = managePriceFilter(items, priceRange);
+    items = sortByPrice(items, sort);
     return items;
-  }, [products, category, search, priceRange]);
+  }, [products, category, search, priceRange, sort]);
 
   if (loading) {
     return <p>Loading products please wait</p>;
@@ -48,9 +54,10 @@ const App = () => {
 
   return (
     <Layout>
-      <div className="w-full bg-white py-8  grid grid-cols-1 md:grid-cols-8 gap-10">
+      <div className="w-full bg-white py-8  grid grid-cols-1 md:grid-cols-10 gap-10">
         <CategoryFilter {...categoryFilterProps} />
         <PriceFilter {...priceFilterProps} />
+        <SortFilter {...SortFilterProps} />
         <InputFilter {...inputFilterProps} />
       </div>
       <main className="md:p-10 bg-slate-100">
