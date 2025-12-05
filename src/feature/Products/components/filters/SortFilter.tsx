cf00664cedeeda091a72e9f0a1303702c23a94b0
androy <androyt1@gmail.com>
2025-12-05 15:58:31 +0000

    the sort filter component

---
+++ src/feature/Products/components/filters/SortFilter.tsx
@@ -0,0 +1,21 @@
+import type { ChangeEvent } from "react";
+
+type SortFilterProp = {
+  sort: string;
+  handleSortOption: (e: ChangeEvent<HTMLSelectElement>) => void;
+};
+
+const SortFilter = ({ sort, handleSortOption }: SortFilterProp) => {
+  return (
+    <select
+      value={sort}
+      onChange={handleSortOption}
+      className="col-span-1 md:col-span-2 px-3 py-2 rounded-xl ring ring-stone-300 shadow-stone-400"
+    >
+      <option value="lowest to highest">Lowest to highest</option>
+      <option value="Highest to lowest">Highest to lowest</option>
+    </select>
+  );
+};
+
+export default SortFilter;
